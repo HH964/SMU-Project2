1f5cca615d7b88689f15d0a2db2edc56156c30b7
Perluceo <FullStackDevSMU@gmail.com>
2019-07-26 22:38:47 -0500

    author changed to user

--- models/items.js
+++ models/items.js
@@ -25,8 +25,8 @@ module.exports = function(sequelize, DataTypes) {
     });
   
     Item.associate = function(models) {
-      // We're saying that a Post should belong to an Author
-      // A Post can't be created without an Author due to the foreign key constraint
+      // We're saying that a Post should belong to an user
+      // A Post can't be created without an user due to the foreign key constraint
       Item.belongsTo(models.User, {
         foreignKey: {
           allowNull: false
